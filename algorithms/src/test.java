e8e5b8e07bf7b91c647612ec5a72755bce2a3570
wangyuwei95 <36839348+wangyuwei95@users.noreply.github.com>
2018-03-24 18:55:07 +0800

    1

---
+++ algorithms/src/test.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.*;
+
+
+public class test {
+    public static void main(String[] args)
+    {
+        System.out.println("i love");
+    }
+}
---
+++ test.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.*;
+
+
+public class test()
+        {
+            public static void main(String[] args)
+            {
+
+            }
+        }
