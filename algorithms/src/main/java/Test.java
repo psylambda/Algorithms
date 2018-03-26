75a453b3dc897097222c357ce968e65b243cf7c0
wangyuwei95 <wangyuwei95@qq.com>
2018-03-26 21:23:37 +0800

    1

--- algorithms/src/main/java/Test.java
+++ algorithms/src/main/java/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args)
     {
         try {
-            Object o = (Object) Class.forName("Abstract").newInstance();
+            Object o =  Class.forName("Abstract").newInstance();
         } catch(Exception e)
         {
             e.printStackTrace();
